7e77b0b0f088100f52e9c18397c20d2d6be64a0e
Iker <ikerhf14@gmail.com>
2026-05-19 13:47:37 +0100

    crear clase Vehiculo

---
+++ src/net/salesianos/abstractfaces/actividad2/Vehiculo.java
@@ -0,0 +1,24 @@
+package net.salesianos.abstractfaces.actividad2;
+
+public abstract class Vehiculo {
+
+    protected String color;
+    protected int numeroPuertas;
+    protected int numeroRuedas;
+    protected String modelo;
+    protected String matricula;
+
+    public Vehiculo(String color, int numeroPuertas, int numeroRuedas, String modelo, String matricula) {
+        this.color = color;
+        this.numeroPuertas = numeroPuertas;
+        this.numeroRuedas = numeroRuedas;
+        this.modelo = modelo;
+        this.matricula = matricula;
+    }
+
+    public abstract void arrancar();
+
+    public abstract void pararMotor();
+
+    public abstract void mover();
+}
