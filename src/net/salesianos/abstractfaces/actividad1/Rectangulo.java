642fc4b31c4ffad5df7c7e5bf69babc782c58a78
Iker <ikerhf14@gmail.com>
2026-05-19 13:29:45 +0100

    Crear clase Rectangulo

---
+++ src/net/salesianos/abstractfaces/actividad1/Rectangulo.java
@@ -0,0 +1,25 @@
+package net.salesianos.abstractfaces.actividad1;
+
+public class Rectangulo implements CalculoGeometrico {
+
+    private double base;
+    private double altura;
+
+    public Rectangulo(double base, double altura) {
+
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+
+        return base * altura;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+
+        return 2 * (base + altura);
+    }
+}
